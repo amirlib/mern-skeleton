6600472f621894eb31eee297fa77fd13c11326d9
Amir <amir.lib@gmail.com>
2021-02-13 10:43:51 +0200

    created ProfileActions component

--- client/components/Profile/Profile.jsx
+++ client/components/Profile/Profile.jsx
@@ -1,17 +1,13 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
 import Divider from '@material-ui/core/Divider';
-import Edit from '@material-ui/icons/Edit';
-import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Person from '@material-ui/icons/Person';
-import DeleteProfile from './DeleteProfile';
+import ProfileActions from './ProfileActions';
 import TitleTypography from '../UI/typographies/TitleTypography';
 import { AuthContext } from '../../contexts/auth.context';
 import { TitleType } from '../../style/types';
@@ -19,6 +15,12 @@ import { TitleType } from '../../style/types';
 const Profile = (props) => {
   const { profile } = props;
   const { user } = useContext(AuthContext);
+
+  const renderProfileActions = () => {
+    if (!user || user._id !== profile._id) return undefined;
+
+    return <ProfileActions />;
+  };
 
   return (
     <>
@@ -40,24 +42,7 @@ const Profile = (props) => {
             secondary={profile.email}
           />
 
-          {
-            user
-            && user._id === profile._id
-            && (
-              <ListItemSecondaryAction>
-                <Link to={`/user/edit/${user._id}`}>
-                  <IconButton
-                    aria-label="Edit"
-                    color="primary"
-                  >
-                    <Edit />
-                  </IconButton>
-                </Link>
-
-                <DeleteProfile userId={user._id} />
-              </ListItemSecondaryAction>
-            )
-          }
+          {renderProfileActions()}
         </ListItem>
 
         <Divider />

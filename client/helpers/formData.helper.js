5c32911356e3e17a85cdd91298f5f41443dd1d37
Amir Liberzon <amir.lib@gmail.com>
2021-03-11 14:40:15 +0200

    added formData.helper.js file to client/helpers folder

---
+++ client/helpers/formData.helper.js
@@ -0,0 +1,20 @@
+import userSchema from '../../schema/user.schema';
+
+const createFormData = (values, model) => {
+  const data = new FormData();
+  const pairs = Object.entries(values);
+
+  pairs.forEach((pair) => {
+    if (pair[1]
+      || (model
+        && model[pair[0]]
+        && !model[pair[0]].options.required)
+    ) data.append(pair[0], pair[1]);
+  });
+
+  return data;
+};
+
+const createProfileFormData = (values) => createFormData(values, userSchema);
+
+export { createProfileFormData };
